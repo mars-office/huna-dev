dbcd72ae1b9e81f76989b21092b039e36921adf6
Alin Florin <alin@qoffice.ro>
2023-10-12 16:54:46 +0300

    -

---
+++ scripts/run_opa.bat
@@ -0,0 +1,2 @@
+@echo off
+opa run -s --addr=127.0.0.1:8181 --ignore=".*" -w ../../huna-opa-policy
---
+++ scripts/run_opa.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+opa run -s --addr=127.0.0.1:8181 --ignore=".*" -w ../../huna-opa-policy
