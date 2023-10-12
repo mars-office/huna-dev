80e08aa2160e6122cdc4410dbfa4595924953f7c
Veronica Dinu <veronica@qoffice.ro>
2023-10-13 01:17:58 +0300

    -

--- scripts/install_huna.bat
+++ scripts/install_huna.bat
@@ -1,4 +1,4 @@
 @echo off
 cd ../../huna
-helm dependency upgrade --skip-refresh
+helm dependency update --skip-refresh
 helm upgrade --install -n huna --create-namespace --values values-local.yaml --values Chart.lock huna .
