7a9b2a9691aea3596bad232af1d8cc6564fa844e
Alin Florin <alin@qoffice.ro>
2023-12-10 13:23:12 +0200

    d

--- scripts/install_huna.sh
+++ scripts/install_huna.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd ../../huna
 helm dependency update --skip-refresh
-helm upgrade --install -n huna --create-namespace --values values-local.yaml --values Chart.lock huna .
+helm upgrade --install -n huna --create-namespace --values values-local.yaml huna .
